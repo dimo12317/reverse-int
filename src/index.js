798e099777164bb685e0c3de3d3721699f07aeab
dimo12317 <79600828+dimo12317@users.noreply.github.com>
2021-03-15 16:11:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function reverse (n) {
-  if(n < 0) return -reverse(-n);
-  return Number(String(n).split('').reverse().join(""));
+  if(n < 0) return reverse(-n);
+  return Number(n.toString().split('').reverse().join(""));
 }
